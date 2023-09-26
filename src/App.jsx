eefdc74337551e74730092936da84470fc4f50ab
ROGERIO JOSE <57935315+Rogerio-17@users.noreply.github.com>
2023-09-26 10:56:32 -0300

    Finalizando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Header } from "./components/Header";
 
 function App() {
-
   return (
-    <h1>Olá mundo</h1>
-  )
+    <div>
+      <Header />
+      <h1>Olá mundo</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import style from "./Header.module.css"
+import logo from "../assets/Logo.svg"
+
+
+export function Header() {
+    return(
+        <header className={style.header}>
+            <img src={logo} alt="" />
+        </header>
+    )
+}
